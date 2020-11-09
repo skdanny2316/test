34153846d575c7c48591e235511053c9664d1a79
skdanny2316 <Danishshaikh2365@gmail.com>
2020-11-09 20:32:25 +0530

    hungry

---
+++ hungry.py
@@ -0,0 +1,11 @@
+hungry = input('Are you hungry')
+
+if hungry.lower() == 'yes':
+	print('eat burger')
+	print('eat pizza')
+	print('eat samosa')
+else:
+	thirsty = input('are you thirsty')
+	if thirsty == 'yes':
+		print('drink water')
+		print('drink milk')
